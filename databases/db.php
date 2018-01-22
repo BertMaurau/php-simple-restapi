2ff88314c1209fd2f8a2f3c4f49b433ba239ee92
Bert Maurau <bert.maurau@live.be>
2018-01-22 21:57:33 +0100

    comments and extra functions

--- databases/db.php
+++ databases/db.php
@@ -8,32 +8,77 @@
 class DB
 {
 
+    // holds the connection with the database
     public static $mysqli;
 
+    /**
+     * Init a new connection
+     * @throws Exception
+     */
     static function init()
     {
         self::$mysqli = new mysqli(Constants::DB_HOST, Constants::DB_USER, Constants::DB_PASS, Constants::DB_NAME);
+        if (!self::$mysqli) {
+            throw new Exception(self::getLastError());
+        }
     }
 
+    /**
+     * Escape the given value
+     * @param any $value
+     * @return any
+     */
     public static function escape($value)
     {
         return self::$mysqli -> real_escape_string($value);
     }
 
+    /**
+     * Get the last inserted ID
+     * @return integer
+     */
     public static function getId()
     {
         return self::$mysqli -> insert_id;
     }
 
+    /**
+     * Get the amount of affected rows
+     * @return integer
+     */
     public static function getAffectedRows()
     {
         return self::$mysqli -> affected_rows;
     }
 
+    /**
+     * Execute a given query
+     * @param string $sql
+     * @return resultset
+     */
     public static function query($sql)
     {
         $result = self::$mysqli -> query($sql);
         return $result;
+    }
+
+    /**
+     * Close the connection (if open)
+     */
+    public static function close()
+    {
+        if (self::$mysqli) {
+            self::$mysqli -> close();
+        }
+    }
+
+    /**
+     * Return the last mysql error
+     * @return string
+     */
+    public static function getLastError()
+    {
+        return self::$mysqli -> error;
     }
 
 }
