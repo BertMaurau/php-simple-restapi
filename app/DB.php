d75fddf8d5d70e0fff5cb3398e0f9115e7b7933e
Bert Maurau <hello@bertmaurau.be>
2018-03-26 23:00:02 +0200

    added database name value

--- app/DB.php
+++ app/DB.php
@@ -12,21 +12,28 @@ class DB
 
     // holds the connection with the database
     private static $mysqli;
+    private static $database;
 
     /**
      * Init a new connection depending on the current environment.
      * The environments can be defined within the app/Env file.
      * @throws Exception
      */
-    static function init()
+    static function init($connectWithDatabase = true)
     {
         switch (Env::getEnv()) {
 
             case Env::ENV_LOCALHOST:
-                self::$mysqli = new mysqli(DB_LOCAL_HOST, DB_LOCAL_USER, DB_LOCAL_PASS, DB_LOCAL_NAME);
+                self::$database = ($connectWithDatabase) ? DB_LOCAL_NAME : null;
+                self::$mysqli = new mysqli(DB_LOCAL_HOST, DB_LOCAL_USER, DB_LOCAL_PASS, self::$database);
                 break;
             case Env::ENV_DEVELOPMENT:
-                self::$mysqli = new mysqli(DB_DEV_HOST, DB_DEV_USER, DB_DEV_PASS, DB_DEV_NAME);
+                self::$database = ($connectWithDatabase) ? DB_DEV_NAME : null;
+                self::$mysqli = new mysqli(DB_DEV_HOST, DB_DEV_USER, DB_DEV_PASS, self::$database);
+                break;
+            case Env::ENV_PRODUCTION:
+                self::$database = ($connectWithDatabase) ? DB_DEV_NAME : null;
+                self::$mysqli = new mysqli(DB_PROD_HOST, DB_PROD_USER, DB_PROD_PASS, self::$database);
                 break;
             /*
              * Add as many environment connects as you want. As long as you have
@@ -98,6 +105,15 @@ class DB
     public static function getLastError()
     {
         return self::$mysqli -> error;
+    }
+
+    /**
+     * Return the current database
+     * @return string database name 
+     */
+    public static function getDatabase()
+    {
+        return self::$database;
     }
 
 }
